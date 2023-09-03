963f3c3cca5e2d686c509d35b52c3ad43083f3e3
Youssef Eltalyawy <yousefeltalyawy@gmail.com>
2023-09-03 21:38:37 +0300

    Added UI and made code more optimal

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-let score = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+const rockBtn = document.querySelector(".rockBtn");
+const paperBtn = document.querySelector(".paperBtn");
+const scissorsBtn = document.querySelector(".scissorsBtn");
+
+let playerSelection = "";
+
+const playerScoreEl = document.querySelector(".player-score");
+const computerScoreEl = document.querySelector(".computer-score");
+playerScoreEl.textContent = playerScore;
+computerScoreEl.textContent = computerScore;
+
+const resultEl = document.querySelector(".result");
+
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
   let choiceIndex = Math.floor(Math.random() * choices.length);
@@ -9,36 +24,58 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   if (playerSelection === "rock" && computerSelection === "Paper") {
-    score = score;
+    computerScore++;
     return "You Lose! Paper beats Rock";
   } else if (playerSelection === "paper" && computerSelection === "Scissors") {
-    score = score;
+    computerScore++;
     return "You Lose! Scissors beats paper";
   } else if (playerSelection === "scissors" && computerSelection === "Rock") {
-    score = score;
+    computerScore++;
     return "You Lose! Rock beats Scissors";
   } else if (playerSelection === "paper" && computerSelection === "Rock") {
-    score++;
+    playerScore++;
     return "You Win! Paper beats Rock";
   } else if (playerSelection === "scissors" && computerSelection === "Paper") {
-    score++;
+    playerScore++;
     return "You Win! Scissors beats paper";
   } else if (playerSelection === "rock" && computerSelection === "Scissors") {
-    score++;
+    playerScore++;
     return "You Win! Rock beats Scissors";
   } else {
-    score = score;
     return "Tie!";
   }
 }
 
+rockBtn.addEventListener("click", function () {
+  playerSelection = "rock";
+  game();
+});
+paperBtn.addEventListener("click", function () {
+  playerSelection = "paper";
+  game();
+});
+scissorsBtn.addEventListener("click", function () {
+  playerSelection = "scissors";
+  game();
+});
+
 function game() {
-  for (i = 0; i < 5; i++) {
-    const playerSelection = prompt("Please select: ")
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+  playerScoreEl.textContent = playerScore
+  computerScoreEl.textContent = computerScore
+  if (playerScore === 5) {
+    resultEl.textContent = "Player wins!";
+    setTimeout(resetGame, 1000); // Wait 5 seconds to reset
+  } else if (computerScore === 5) {
+    resultEl.textContent = "Computer wins!";
+    setTimeout(resetGame, 1000);
   }
-  console.log(score)
 }
 
-game();
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  playerScoreEl.textContent = 0;
+  computerScoreEl.textContent = 0;
+}
